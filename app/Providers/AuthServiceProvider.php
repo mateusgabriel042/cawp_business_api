29c2446d7cc43375cf96a0a85a8f9fa23a5a7007
mateusgabriel042 <mateus.gabriel042@gmail.com>
2022-03-20 15:33:35 -0300

    criação da base de autenticação

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,6 +4,9 @@ namespace App\Providers;
 
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\DB;
+use App\Models\Permission;
+use App\Models\User;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -25,6 +28,21 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+        if(DB::getSchemaBuilder()->hasTable('permissions')){
+
+            $permissions = Permission::with('roles')->get();
+
+            foreach ($permissions as $permission) {
+
+                Gate::define($permission->name, function(User $user) use ($permission){
+                    return $user->hasPermission($permission);
+                });
+            }
+
+            Gate::before(function(User $user, $ability){
+                if($user->hasAnyRoles('admin'))
+                    return true;
+            });
+        }
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,15 +5,11 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
-{
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
+class DatabaseSeeder extends Seeder {
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $this->call([
+	        RoleSeeder::class,
+	    ]);
     }
 }
